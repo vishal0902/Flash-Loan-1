536c2fa871cf1db31aecad1195982aa33c0a12a5
Vishal Srivastava <eth.vishal@gmail.com>
2022-07-20 21:19:12 +0530

    Wrote test to check the contract's funtion are working

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,12 +1,13 @@
-import "@nomicfoundation/hardhat-toolbox";
+require('@nomicfoundation/hardhat-toolbox');
 require("dotenv").config({path:".env"});
 
-/** @type import('hardhat/config').HardhatUserConfig */
-export const solidity = "0.8.10";
-export const networks = {
-  hardhat: {
-    forking: {
-      url: process.env.ALCHEMY_API_KEY_URL,
+module.exports = {
+  solidity: "0.8.10",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ALCHEMY_API_KEY_URL,
+      },
     },
   },
 };
